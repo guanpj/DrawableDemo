2b5d1ad13ef1dbab122808513c00e9d7c78651ee
guanpj <guanpj.me@gmail.com>
2019-06-18 15:39:38 +0800

    111

---
+++ app/src/main/java/com/me/guanpj/drawable/demo/GradientDrawableActivity.kt
@@ -0,0 +1,12 @@
+package com.me.guanpj.drawable.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GradientDrawableActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bitmap_drawable)
+    }
+}
--- app/src/main/java/com/me/guanpj/drawable/demo/MainActivity.kt
+++ app/src/main/java/com/me/guanpj/drawable/demo/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.me.guanpj.drawable.demo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_bitmap_drawable).setOnClickListener {
+            startActivity(Intent(MainActivity@this, BitmapDrawableActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btn_gradient_drawable).setOnClickListener {
+            startActivity(Intent(MainActivity@this, GradientDrawableActivity::class.java))
+        }
     }
 }
