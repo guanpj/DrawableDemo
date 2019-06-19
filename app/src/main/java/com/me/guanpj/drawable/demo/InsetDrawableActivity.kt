c777cdc2d8f53a124acfe8da866abfbc2a5aeda1
guanpj <guanpj.me@gmail.com>
2019-06-19 18:07:03 +0800

    111

--- app/src/main/java/com/me/guanpj/drawable/demo/InsetDrawableActivity.kt
+++ app/src/main/java/com/me/guanpj/drawable/demo/InsetDrawableActivity.kt
@@ -1,10 +1,6 @@
 package com.me.guanpj.drawable.demo
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
-import android.widget.Button
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class InsetDrawableActivity : AppCompatActivity() {
