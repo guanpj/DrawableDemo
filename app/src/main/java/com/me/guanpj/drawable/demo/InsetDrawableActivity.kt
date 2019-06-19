4a6608ebb7ba14b3272864b322885f8b07bc28fb
guanpj <guanpj.me@gmail.com>
2019-06-19 17:09:16 +0800

    111

---
+++ app/src/main/java/com/me/guanpj/drawable/demo/InsetDrawableActivity.kt
@@ -0,0 +1,14 @@
+package com.me.guanpj.drawable.demo
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class InsetDrawableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inset_drawable)
+    }
+}
