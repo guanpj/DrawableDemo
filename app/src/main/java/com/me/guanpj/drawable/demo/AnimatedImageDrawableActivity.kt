caa8cc20103a2a5ca14eacc1abaaeff263cb20b3
guanpj <guanpj.me@gmail.com>
2019-06-29 17:52:02 +0800

    111

--- app/src/main/java/com/me/guanpj/drawable/demo/AnimatedImageDrawableActivity.kt
+++ app/src/main/java/com/me/guanpj/drawable/demo/AnimatedImageDrawableActivity.kt
@@ -1,16 +1,12 @@
 package com.me.guanpj.drawable.demo
 
-import android.content.Context
 import android.graphics.ImageDecoder
 import android.graphics.drawable.Animatable2
-import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_animated_image_drawable.*
-import kotlinx.coroutines.*
 
 class AnimatedImageDrawableActivity : AppCompatActivity() {
     private val cacheAsset: CacheAsset by lazy {
@@ -21,35 +17,16 @@ class AnimatedImageDrawableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animated_image_drawable)
-        //image.setImageDrawable(resources.getDrawable(R.drawable.ic_arrow))
+
         button.setOnClickListener {
-            //Glide.with(this@AnimatedImageDrawableActivity).load(R.drawable.giphy).into(image)
-            var drawable = ImageDecoder.decodeDrawable(ImageDecoder.createSource(resources, R.drawable.giphy))
-            image.setImageDrawable(drawable)
-            if(drawable is Animatable2) {
-                drawable.start()
+            ImageDecoder.createSource(cacheAsset.file("webp_example.webp")).also { source ->
+                ImageDecoder.decodeDrawable(source).also { drawable ->
+                    image.setImageDrawable(drawable)
+                    if(drawable is Animatable2) {
+                        drawable.start()
+                    }
+                }
             }
-            /*ImageDecoder.createSource(resources, R.drawable.giphy).also { source ->
-                ImageDecoder.decodeDrawable(source).also { drawable ->
-                    image.setImageDrawable(drawable)
-                    if(drawable is Animatable2) {
-                        drawable.start()
-                    }
-                }
-            }*/
-            /*ImageDecoder.createSource(cacheAsset.file("giphy.gif")).also { source ->
-                ImageDecoder.decodeDrawable(source).also { drawable ->
-                    image.setImageDrawable(drawable)
-                    if(drawable is Animatable2) {
-                        drawable.start()
-                    }
-                }
-            }*/
-            /*ImageDecoder.createSource(cacheAsset.file("StylingAndroid.png")).also { source ->
-                ImageDecoder.decodeBitmap(source).also { bitmap ->
-                    image.setImageBitmap(bitmap)
-                }
-            }*/
         }
     }
 }
