589212999f63e513b4028cc3ea39f57046fcb470
guanpj <guanpj.me@gmail.com>
2019-07-01 10:08:34 +0800

    111

--- app/src/main/java/com/me/guanpj/drawable/demo/AnimatedImageDrawableActivity.kt
+++ app/src/main/java/com/me/guanpj/drawable/demo/AnimatedImageDrawableActivity.kt
@@ -1,32 +1,40 @@
 package com.me.guanpj.drawable.demo
 
 import android.graphics.ImageDecoder
-import android.graphics.drawable.Animatable2
+import android.graphics.drawable.AnimatedImageDrawable
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_animated_image_drawable.*
 
+@RequiresApi(Build.VERSION_CODES.P)
 class AnimatedImageDrawableActivity : AppCompatActivity() {
+    var mAnimatedImageDrawable: AnimatedImageDrawable? = null
+
     private val cacheAsset: CacheAsset by lazy {
         CacheAsset(this)
     }
 
-    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animated_image_drawable)
 
         button.setOnClickListener {
-            ImageDecoder.createSource(cacheAsset.file("webp_example.webp")).also { source ->
+            ImageDecoder.createSource(cacheAsset.file("gif_example.gif")).also { source ->
                 ImageDecoder.decodeDrawable(source).also { drawable ->
                     image.setImageDrawable(drawable)
-                    if(drawable is Animatable2) {
+                    if(drawable is AnimatedImageDrawable) {
+                        mAnimatedImageDrawable = drawable
                         drawable.start()
                     }
                 }
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mAnimatedImageDrawable?.run { stop() }
     }
 }
